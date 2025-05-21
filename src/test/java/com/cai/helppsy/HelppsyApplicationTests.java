c3ec3448b77cd3dc85c046dd5579c26b430e0f95
hoojin <none@gmail.com>
2025-05-21 12:36:45 +0900

    이제 되려나....

---
+++ src/test/java/com/cai/helppsy/HelppsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cai.helppsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelppsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
