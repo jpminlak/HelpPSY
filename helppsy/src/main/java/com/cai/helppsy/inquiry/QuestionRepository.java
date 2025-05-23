82720ed2c8baef6e07b2cc88c5e17d733f86da8a
yb2006 <yunbin1117@gmail.com>
2025-05-23 17:55:38 +0900

    회원 문이 상세 보기 및 답변 작성 완료

---
+++ helppsy/src/main/java/com/cai/helppsy/inquiry/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.cai.helppsy.inquiry;
+
+import ch.qos.logback.core.rolling.helper.IntegerTokenConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    void deleteById(Integer id);
+}
