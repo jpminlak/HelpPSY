4f8045e4e3bfa81b149305d7de7ec69a185690fd
jpminlak <hrk866@naver.com>
2025-05-26 10:43:06 +0900

    지도 API 기초 & html 문서 부분 넣기(fragment)

--- helppsy/src/main/java/com/cai/helppsy/StartController.java
+++ helppsy/src/main/java/com/cai/helppsy/StartController.java
@@ -9,17 +9,27 @@ public class StartController {
     @GetMapping("/")
     public String start(){
         System.out.println("메인 페이지 시작");
-        return "start";
+        return "mainPage";
     }
 
+    @GetMapping("/free")
+    public String free(){
+        System.out.println("자유 게시판");
+        return "free";
+    }
+    @GetMapping("/accident")
+    public String accident(){
+        System.out.println("사고 게시판");
+        return "accident";
+    }
+    @GetMapping("/inquiry")
+    public String inquiry(){
+        System.out.println("문의하기");
+        return "inquiry";
+    }
     @GetMapping("/map")
     public String map(){
         System.out.println("지도 페이지 입장");
         return "/map/map";
-    }
-    @GetMapping("/2dmap")
-    public String map2d(){
-        System.out.println("2D 지도 페이지 입장");
-        return "/map/2dmap";
     }
 }
