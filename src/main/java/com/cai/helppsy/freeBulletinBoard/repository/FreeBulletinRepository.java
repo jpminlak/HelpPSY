fc42681d1ee4774b4d86986aa0e877ab180147f5
hongminwoo95 <als12dn@naver.com>
2025-06-24 10:32:24 +0900

    06.23(월) 오후 11:30 ~ 06.24(화) 오전 2:11 : 댓글,대댓글,게시글 해당되는 프로필 이미지 회원이 이미지 변경할때마다 변경되도록 DTO객체를 리턴하는 방식으로 전체적인 수정,  대댓글 클릭시 프로필로 이동하기 추가

--- src/main/java/com/cai/helppsy/freeBulletinBoard/repository/FreeBulletinRepository.java
+++ src/main/java/com/cai/helppsy/freeBulletinBoard/repository/FreeBulletinRepository.java
@@ -3,5 +3,8 @@ package com.cai.helppsy.freeBulletinBoard.repository;
 import com.cai.helppsy.freeBulletinBoard.entity.FreeBulletin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FreeBulletinRepository extends JpaRepository<FreeBulletin, Integer> {
+    List<FreeBulletin> findByWriter(String writer); // 작성자(alias)로 게시물 목록 가져오기
 }
--- src/main/java/com/cai/helppsy/memberManager/signupService.java
+++ src/main/java/com/cai/helppsy/memberManager/signupService.java
@@ -1,5 +1,6 @@
 package com.cai.helppsy.memberManager;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +11,13 @@ public class signupService {
     public void signup(SignupEntity sinupentity) { // 회원가입
         signupRepository.save(sinupentity);
     }   // 회원 가입
+
     public SignupEntity login(String userId) {
         return signupRepository.findByuserId(userId);
+
     }   // 회원 별칭 가져오기 (세션유지용)
+
+
     public boolean existsById(String userId) {
         return signupRepository.existsByUserId(userId); // 직접 선언한 메서드 호출
     }
